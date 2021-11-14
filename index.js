06a7ba4a5527d462b7a6b1a4dada71611233864a
mdcu.firebase@chula.md <>
2021-11-14 22:11:52 +0700

    index+

---
+++ index.js
@@ -0,0 +1,7 @@
+await.liff.init({liffId:"1656631126-wg1J4QWV"})
+
+switch(liff.getOS()){
+  case "android":body.style.backgroundColor = "green";break
+  case "ios":body.style.backgroundColor = "pink";break
+  case "web":body.style.backgroundColor ="black";break
+}
