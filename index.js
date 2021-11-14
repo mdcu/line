4fe44184616554609cea19d479c857749180c48c
mdcu.firebase@chula.md <>
2021-11-14 22:22:53 +0700

    index+3

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-await.liff.init({liffId:"1656631126-wg1J4QWV"})
+await liff.init({liffId:"1656631126-wg1J4QWV"})
 
 switch(liff.getOS()){
-  case "android":body.style.backgroundColor = "green";break
-  case "ios":body.style.backgroundColor = "pink";break
-  case "web":body.style.backgroundColor ="black";break
+  case "android":document.body.style.backgroundColor = "green";break
+  case "ios":document.body.style.backgroundColor = "pink";break
+  case "web":document.body.style.backgroundColor ="black";break
+}
+
+async function getProfile(){
+  let profile = await liff.getProfile()
+  document.body.appendChild(document.createElement("p")).innerText = profile.userId
+  document.body.appendChild(document.createElement("p")).innerText = profile.statusMessage
+  document.body.appendChild(document.createElement("p")).innerText = profile.displayName
+  document.body.appendChild(document.createElement("img")).src = profile.pictureUrl
+}
+
+async function main(){
+
+  getProfile();
 }
